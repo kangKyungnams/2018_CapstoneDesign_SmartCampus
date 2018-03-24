8bdd2ca5812074c1c9033a89ba61c4e212278f93
bgjuw12 <33053185+bgjuw12@users.noreply.github.com>
2018-03-24 19:56:35 +0900

    로그인, 회원가입, 블루투스
    1차 단계
    수정필요

--- app/src/main/java/com/capston/iceamericano/smartcampus/LoginActivity.java
+++ app/src/main/java/com/capston/iceamericano/smartcampus/LoginActivity.java
@@ -1,25 +1,122 @@
 package com.capston.iceamericano.smartcampus;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    String TAG = "LoginActivity";
+    private FirebaseAuth mAuth;
+    private EditText ed_ID;
+    private EditText ed_PW;
+    private Button registerButton;
+    private Button loginButton;
+
+    BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ed_ID = (EditText) findViewById(R.id.ed_ID);
+        ed_PW = (EditText) findViewById(R.id.ed_PW);
+        registerButton = (Button) findViewById(R.id.registerButton);
+        loginButton = (Button) findViewById(R.id.loginButton);
 
-        TextView registerButton = (TextView)findViewById(R.id.registerButton);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                LoginActivity.this.startActivity(registerIntent);
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        blueTooth();
+
+        registerButton.setOnClickListener(register);
+        loginButton.setOnClickListener(login);
+
+
+
     }
+
+
+    void blueTooth ()
+    {
+        if(mBluetoothAdapter == null)
+        {
+            Log.d(TAG, "There is no bluetooth");
+        }
+        else
+        {
+            if(mBluetoothAdapter.isEnabled())
+            {
+                Log.d(TAG, "Bluetooth is already on");
+            }
+            else
+            {
+                mBluetoothAdapter.enable();
+            }
+        }
+    }
+
+
+    Button.OnClickListener register = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) { // 버튼에 로그인 기능 입히기
+            Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+            LoginActivity.this.startActivity(registerIntent);
+        }
+    };
+
+    Button.OnClickListener login = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) { // 빈칸일시 에러메시지 띄우기
+            if (ed_ID.getText().toString().isEmpty() == true) {
+                    ed_ID.setError("Please confirm the Email");
+                return;
+            }
+            if (ed_PW.getText().toString().isEmpty() == true) {
+                    ed_PW.setError("PW를 입력해 주세요");
+                return;
+            }
+            loginAuth(ed_ID.getText().toString(),ed_PW.getText().toString());
+        }
+    };
+
+
+
+    void loginAuth(String email, String password)//로그인 기능
+    {
+
+        final Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            LoginActivity.this.startActivity(loginIntent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "아이디와 비밀번호를 다시 확인해주세요.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+
 }
--- app/src/main/java/com/capston/iceamericano/smartcampus/RegisterActivity.java
+++ app/src/main/java/com/capston/iceamericano/smartcampus/RegisterActivity.java
@@ -1,34 +1,98 @@
 package com.capston.iceamericano.smartcampus;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
     private ArrayAdapter adapter;
     private Spinner spinner;
+    private EditText ed_StuNum;
+    private EditText ed_name;
+    private EditText ed_password;
+    private EditText ed_email;
+    private Button bt_signup;
+    String TAG = "RegisterActivity";
+
+    private FirebaseAuth mAuth;
+    DatabaseReference uReference = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference userdata = uReference.child("user");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        ed_StuNum = (EditText)findViewById(R.id.ed_StuNum);
+        ed_name = (EditText)findViewById(R.id.ed_name);
+        ed_password = (EditText)findViewById(R.id.ed_password);
+        ed_email = (EditText)findViewById(R.id.ed_email);
+        bt_signup = (Button)findViewById(R.id.bt_signup);
+
         spinner = (Spinner)findViewById(R.id.Major_Spinner);
         adapter = ArrayAdapter.createFromResource(this, R.array.major, android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-        TextView registerButton2 = (TextView)findViewById(R.id.registerButton2);
-        registerButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent registerIntent2 = new Intent(RegisterActivity.this, MainActivity.class);
-                RegisterActivity.this.startActivity(registerIntent2);
-            }
-        });
+
+        mAuth = FirebaseAuth.getInstance();
+        bt_signup.setOnClickListener(on_click);
     }
+
+
+    Button.OnClickListener on_click = new Button.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            createAccount(ed_email.getText().toString(), ed_password.getText().toString());
+            finish();
+        }
+    };
+
+    public void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser User = task.getResult().getUser();
+                            userdata.child(ed_StuNum.getText().toString()).child("ID").setValue(ed_email.getText().toString());
+                            userdata.child(ed_StuNum.getText().toString()).child("PW").setValue(ed_password.getText().toString());
+                            userdata.child(ed_StuNum.getText().toString()).child("Name").setValue(ed_name.getText().toString());
+//                            userdata.child(User.getUid()).child("Photo").setValue("https://firebasestorage.googleapis.com/v0/b/anypeople-12.appspot.com/o/standard.png?alt=media&token=b2e19a52-39c8-44af-9989-d73e5c5c30d1");
+                            userdata.child(ed_StuNum.getText().toString()).child("keyUID").setValue(User.getUid().toString());
+                            Toast.makeText(RegisterActivity.this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "이미 있는 ID 입니다.", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 }
