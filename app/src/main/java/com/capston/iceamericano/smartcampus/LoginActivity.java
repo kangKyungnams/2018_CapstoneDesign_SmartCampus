9498c4b7d34dcc404574c613be601a8d684861c8
bgjuw12 <33053185+bgjuw12@users.noreply.github.com>
2018-03-30 19:59:25 +0900

    비밀번호 찾기
    이메일을 통한 비밀번호 찾기
    
    멘트 수정 전

--- app/src/main/java/com/capston/iceamericano/smartcampus/LoginActivity.java
+++ app/src/main/java/com/capston/iceamericano/smartcampus/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.capston.iceamericano.smartcampus;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +9,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v7.app.AlertDialog;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -22,7 +26,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 
-
 public class LoginActivity extends AppCompatActivity {
 
     String TAG = "LoginActivity";
@@ -31,6 +34,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText ed_PW;
     private Button registerButton;
     private Button loginButton;
+    private Button PWmod_Button;
 
     DatabaseReference uReference = FirebaseDatabase.getInstance().getReference();
     DatabaseReference userdata = uReference.child("user");
@@ -44,18 +48,76 @@ public class LoginActivity extends AppCompatActivity {
         ed_PW = (EditText) findViewById(R.id.ed_PW);
         registerButton = (Button) findViewById(R.id.registerButton);
         loginButton = (Button) findViewById(R.id.loginButton);
+        PWmod_Button = (Button) findViewById(R.id.PWmod_Button);
 
         mAuth = FirebaseAuth.getInstance();
-
 
         registerButton.setOnClickListener(register);
         loginButton.setOnClickListener(login);
 
 
+
+        //비밀번호 찾기 버튼 레이아웃
+        PWmod_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout layout = new LinearLayout(LoginActivity.this);
+                layout.setOrientation(LinearLayout.VERTICAL);
+                final TextView description3 = new TextView(LoginActivity.this);
+                description3.setText(" ");
+                final TextView description2 = new TextView(LoginActivity.this);
+                description2.setText("    가입시 이용한 학번과 E-mail을 입력하세요. ");
+                description2.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                final EditText etId = new EditText(LoginActivity.this);
+                etId.setHint("   학 번 ");
+                final EditText etMail = new EditText(LoginActivity.this);
+                etMail.setHint("   E-mail ");
+                final TextView description = new TextView(LoginActivity.this);
+                description.setText("    입력하신 E-mail로 비밀번호가 전송됩니다. ");
+                description.setTextColor(getResources().getColor(R.color.colorrealGray));
+                description2.setTextColor(getResources().getColor(R.color.colorrealGray));
+
+                layout.addView(description3);
+                layout.addView(description2);
+                layout.addView(etId);
+                layout.addView(etMail);
+                layout.addView(description);
+
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(LoginActivity.this);
+
+                dialog.setTitle(" 비밀번호 찾기 ")
+                        .setView(layout)
+                        .setPositiveButton("확인  ", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String email_address = etMail.getText().toString();
+                                if (etMail.getText().toString().isEmpty() == true) {
+                                    etMail.setError("이메일을 입력해 주세요.");
+                                    return;
+                                }
+                                FirebaseAuth auth = FirebaseAuth.getInstance();
+                                auth.sendPasswordResetEmail(email_address).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+
+                                    }
+                                });
+
+                            }
+                        })
+                        .setNeutralButton("  취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+        });
+
+
     }
-
-
-
 
 
     Button.OnClickListener register = new Button.OnClickListener() {
@@ -78,7 +140,6 @@ public class LoginActivity extends AppCompatActivity {
                     ed_PW.setError("PW를 입력해 주세요");
                 return;
             }
-
 
 
             userdata.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -105,7 +166,6 @@ public class LoginActivity extends AppCompatActivity {
     };
 
 
-
     void loginAuth(String email, String password)//로그인 기능
     {
 
@@ -129,6 +189,5 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 });
     }
-
 
 }
