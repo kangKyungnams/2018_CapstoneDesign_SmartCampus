355fc434de9a380efc153dcd0775853a8ac51a30
coolcow7808 <37401712+coolcow7808@users.noreply.github.com>
2018-06-12 20:27:37 +0900

    교수용 강의정보 업뎃
    업뎃

---
+++ app/src/main/java/com/capston/iceamericano/smartcampus/InfoCourseProf.java
@@ -0,0 +1,13 @@
+package com.capston.iceamericano.smartcampus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoCourseProf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_course_prof);
+    }
+}
